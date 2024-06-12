7f2f6ebfbb2a8d09c9257fff5386a138a65caf56
rpeight8 <vitali.shuman96@gmail.com>
2024-06-12 14:31:23 +0300

    CORS for ionic

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 import toolboxRoutes from './routes/toolbox.route';
 import toolRoutes from './routes/tool.route';
 import utilRoutes from './routes/util.route';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      'capacitor://localhost',
+      'ionic://localhost',
+      'http://localhost',
+      'http://localhost:8080',
+      'http://localhost:8100',
+    ], // Allow only this origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+  })
+);
 
 app.use(express.json());
 
